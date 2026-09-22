20cc5835881761e343c90c542bde7d8422a9f12f
rcxyzjs <lordrx50@gmail.com>
2026-09-22 11:24:33 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     BOT_NAME: 'ZyroX',
     BOT_TOKEN: process.env.BOT_TOKEN,
-    CLIENT_ID: process.env.CLIENT_ID,'
+    CLIENT_ID: process.env.CLIENT_ID,
     OWNER_ID: '1510291022578716903', // owner-only commands
 
     PREFIX: ',', // default text command prefix
