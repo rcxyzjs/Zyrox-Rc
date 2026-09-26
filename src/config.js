048b6949dfcb6b76662be791500b41e01ea1fe97
rcxyzjs <lordrx50@gmail.com>
2026-09-26 21:04:25 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ module.exports = {
     BOT_NAME: 'ZyroX',
     BOT_TOKEN: process.env.BOT_TOKEN,
     CLIENT_ID: process.env.CLIENT_ID,
-    OWNER_ID: '1068553767504588911', // owner-only commands
+    OWNER_ID: '1372858321941299231', // owner-only commands
     PREFIX: '&', // default text command prefix
 
     STATUS: {
